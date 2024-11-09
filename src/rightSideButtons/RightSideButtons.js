d72f17fbe129a330ef6e1845c0ff87fa9cbb752d
LeeJay0126 <topaz9889@gmail.com>
2024-11-09 13:07:11 -0800

    Update RightSideButtons.js

---
+++ src/rightSideButtons/RightSideButtons.js
@@ -0,0 +1,11 @@
+
+const RightSideButtons = () => [
+    ["mc","m+","m-","mr"],
+    ["c","+/-","/","x"],
+    ["7","8","9","-"],
+    ["4","5","6","+"],
+    ["1","2","3"],
+    ["0","."]
+];
+
+export default RightSideButtons;
