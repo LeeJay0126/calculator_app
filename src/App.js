e2ee81e96f10812d078cfe019a3a8bd9e9432ec7
LeeJay0126 <topaz9889@gmail.com>
2024-11-14 11:20:25 -0800

    2 contexts added
    2 additional contexts added for memory and result section

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import './App.css';
 import CalculatorFrame from './displaySection/CalculatorFrame';
 import { DisplayContext } from './components/DisplayContext';
 import { useState } from 'react';
+import { MemoryContext } from './components/MemoryContext';
+import { ResultContext } from './components/ResultContext';
 
 function App() {
 
   const [display, setDisplay] = useState("");
+  const [memory, setMemory] = useState(0);
+  const [result, setResult] = useState("");
 
   return (
     <DisplayContext.Provider value={{ display, setDisplay }}>
-      <div className="App">
-        <CalculatorFrame />
-      </div>
+      <MemoryContext.Provider value={{ memory, setMemory }}>
+        <ResultContext.Provider value={{ result, setResult }}>
+          <div className="App">
+            <CalculatorFrame />
+          </div>
+        </ResultContext.Provider>
+      </MemoryContext.Provider>
     </DisplayContext.Provider>
   );
 }
